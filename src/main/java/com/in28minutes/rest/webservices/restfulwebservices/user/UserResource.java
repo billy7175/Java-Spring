56c90bfe7625d6b43994fd00d0512083a91f27ca
LAPTOP-C71E3L3P\Billy <whgus439@gmail.com>
2022-02-10 22:54:24 +0900

    /users/{id} findOne

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserResource.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserResource.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -19,4 +20,9 @@ public class UserResource {
 		return service.findAll();
 	}
 		//retrieveUser(int id)
+	
+	@GetMapping("/users/{id}")
+	private User retrieveUser(@PathVariable int id) {
+		return service.findOne(id);
+	}
 }
